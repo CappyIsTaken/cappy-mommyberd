e30fad97eac9f5694928d58d72de367988d6ca85
Alen Kapitanker <alengamer76@gmail.com>
2022-08-24 13:36:45 +0300

    test1

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const express = require("express")
 const app = express()
 
 
+app.get("/login", async (req,res) => {
+    await client.login(process.env.TOKEN)
+    setupCommands()
+    res.send("Successfully logged in!")
+})
+
+
 app.get("/", (req, res) => {
     res.send("HELLO!!!")
 })
-(async() => {
-    await client.login(process.env.TOKEN)
-    setupCommands()
-    app.listen(process.env.PORT || 3000)
-
-})()
+app.listen(process.env.PORT || 3000)

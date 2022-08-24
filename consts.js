5b91944528eb783b7c73e717abcd5dc96ac718a0
Alen Kapitanker <alengamer76@gmail.com>
2022-08-24 13:41:14 +0300

    xD

--- consts.js
+++ consts.js
@@ -11,7 +11,7 @@ let client = new Client({
        GatewayIntentBits.GuildVoiceStates,
        GatewayIntentBits.GuildMembers,
        GatewayIntentBits.Guilds,
-       GatewayIntentBits.MessageContent
+       GatewayIntentBits.GuildMessageTyping,
     ],
 })
 

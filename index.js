18c79483b263a47d1fbd2593751d137cdba045f8
Alen Kapitanker <alengamer76@gmail.com>
2022-08-24 13:05:08 +0300

    ssssssss

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express()
 app.get("/", (req, res) => {
     res.send("HELLO!!!")
 })
-client.login(process.env.TOKEN)
-setupCommands()
-app.listen(process.env.PORT || 3000)
+(async() => {
+    await client.login(process.env.TOKEN)
+    setupCommands()
+    app.listen(process.env.PORT || 3000)
 
+})

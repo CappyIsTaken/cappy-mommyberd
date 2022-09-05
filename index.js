6555bbfed5b8e968f1ad2fee229edc75d2804a9d
RioUwU <109903721+CappyIsTaken@users.noreply.github.com>
2022-09-05 13:03:56 +0000

    fixed!

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {client, distube, setupCommands} = require("./consts")
+const { client, distube, setupCommands } = require("./consts")
 const express = require("express")
 require("dotenv").config()
 const cron = require("node-cron")
@@ -12,32 +12,32 @@ const app = express()
 
 
 app.get("/", (req, res) => {
-    res.send("HELLO!!!")
+  res.send("HELLO!!!")
 })
 
 app.listen(process.env.PORT || 3000, async () => {
-    await client.login(process.env.TOKEN)
-    setupCommands()
-    cron.schedule("0 4 * * *", async () => {
-        console.log("starting cron job!")
-        const ids = await utils.getDiscordIds()
-        const me = await client.users.fetch("279956479140954113", false)
-        ids.forEach(async id => {
-            console.log("Now doing this account! " + id)
-            const accLogin = await utils.loginToFortnite(id)
-            const claimResp = await utils.claimDailyReward(id, accLogin)
-            const user = await client.users.fetch(id, false)
-            if(claimResp.items.length > 0) {
-                const reward = claimResp.items[0]
-                const displayName = await utils.getDisplayName(rewar)
-                await user.send(`Hello there, ${user}!\nToday you got: ${reward.quantity}x ${displayName}`)
-                await me.send(`${accLogin.displayName} got today: ${reward.quantity}x ${displayName}`)
-            }
-        })
-            
-        }, {
-            timezone: "Asia/Jerusalem"
-        })
+  await client.login(process.env.TOKEN)
+  setupCommands()
+  cron.schedule("0 4 * * *", async () => {
+    console.log("starting cron job!")
+    const ids = await utils.getDiscordIds()
+    const me = await client.users.fetch("279956479140954113", false)
+    ids.forEach(async id => {
+      console.log("Now doing this account! " + id)
+      const accLogin = await utils.loginToFortnite(id)
+      const claimResp = await utils.claimDailyReward(id, accLogin)
+      const user = await client.users.fetch(id, false)
+      if (claimResp.items.length > 0) {
+        const reward = claimResp.items[0]
+        const displayName = await utils.getDisplayName(reward)
+        await user.send(`Hello there, ${user}!\nToday you got: ${reward.quantity}x ${displayName}`)
+        await me.send(`${accLogin.displayName} got today: ${reward.quantity}x ${displayName}`)
+      }
+    })
+
+  }, {
+    timezone: "Asia/Jerusalem"
+  })
 })
 
 
